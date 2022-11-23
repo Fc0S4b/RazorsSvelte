e9a2739b91ab3567c83959cf7d454ec3751ea7db
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-22 21:20:59 -0300

    setup

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  // pages
+  import Login from './pages/Login.svelte';
+  import Checkout from './pages/Checkout.svelte';
+  import About from './pages/About.svelte';
+  import Home from './pages/Home.svelte';
+  import Products from './pages/Products.svelte';
+  import ProductsTemplate from './pages/ProductsTemplate.svelte';
+</script>
+
+<h1>Hello</h1>
