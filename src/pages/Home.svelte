7eeb9cea7f2c8cce031fa10c3a754cdd1ae9362a
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-23 15:52:54 -0300

    Products Store Setup

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,16 +1,12 @@
 <script>
   import Hero from '../components/Hero.svelte';
-  // import { Link } from 'svelte-routing';
+
   import { link } from 'svelte-routing';
-  // featured products
-  // function getProps() {
-  //   return { class: 'btn btn-primary btn-hero' };
-  // }
+
 </script>
 
-<!-- varias opciones para pasar estilos al componente, una directamente a class, otra usando getProps con Link y otra usando <a> con link  -->
+
 <Hero>
-  <!-- <Link to="/products" class='btn btn-primary btn-hero'>shop now</Link> -->
-  <!-- <Link to="/products" {getProps}>shop now</Link> -->
+
   <a href="/products" class="btn btn-primary btn-hero " use:link>shop now</a>
 </Hero>
