15d2ad2e92aff1f796bd934c6dd0f8cbf58558df
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-27 23:28:25 -0300

    Toggle Navbars

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,7 +1,16 @@
 <script>
   import SmallNavbar from './SmallNavbar.svelte';
   import BigNavbar from './BigNavbar.svelte';
+  let screenWidth;
+  // onMount
+  // window.add.....
+  // onDestroy
 </script>
 
-<SmallNavbar />
-<BigNavbar />
+<svelte:window bind:innerWidth={screenWidth} />
+
+{#if screenWidth > 992}
+  <BigNavbar />
+{:else}
+  <SmallNavbar />
+{/if}
