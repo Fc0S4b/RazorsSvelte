8eaaaa7b45cfc593309ac591de58959a8bb8ca7a
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-29 15:47:56 -0300

    Cart Basics

---
+++ src/components/Cart/Cart.svelte
@@ -0,0 +1,17 @@
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
+<h2>cart</h2>
